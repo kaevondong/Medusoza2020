d044a96e72a3f6d9155ad9b03a99a864c0e05e4c
Gary Isufi <isufi.2@buckeyemail.osu.edu>
2020-11-15 00:31:21 -0500

    json file created

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -10,20 +10,27 @@ class UploadController < ApplicationController
     require 'net/http'
     require 'json'
     begin
-        uri = URI('https://18.191.45.207:5000/api/models/predict/pole')
-        http = Net::HTTP.new(uri.host, uri.port)
-        request = Net::HTTP::Post.new(uri.path)
-        # req.body = {"image" => params[:attachment]}.to_json
-        # res = http.request(req)
-        # puts "response #{res.body}"
-        # puts JSON.parse(res.body)
-
-        form_data = [['image', File.open(params[:attachment].tempfile)]]
+      uri = URI('https://18.191.45.207:5000/api/models/predict/pole')
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Post.new(uri.path)
+      bounding_boxes = "{"
+      
+      params[:attachment].each do |image|
+        form_data = [['image', File.open(image.tempfile)]]
         request.set_form form_data, 'multipart/form-data'
         response = http.request(request)
+
+        bounding_boxes += "\n\"" + image.original_filename + response.read_body[3,(response.read_body.length - 5)] + ","
         puts response.read_body
+      end
+
+      bounding_boxes = bounding_boxes[0,bounding_boxes.length-1]
+      File.open('bounding_boxes.json', 'w') do |f|
+        f.write(bounding_boxes + "\n}")
+      end
+
     rescue => e
-        puts "failed #{e}"
+      puts "failed #{e}"
     end
     redirect_back(fallback_location: root_path)
   end
