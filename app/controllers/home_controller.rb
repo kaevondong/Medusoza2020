0228a5cafd388b3d3a636b38e7b4cc3e19a73ff5
kaevondong <dong.696@osu.edu>
2020-11-15 02:37:17 -0500

    better parsing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,19 +3,17 @@ class HomeController < ApplicationController
     @title = 'Pole Map'
     poles = []
     Dir.foreach "#{Rails.root}/lib/assets/" do |json_file|
-      pole_data = { xcoord: 'x', ycoord: 'y', poles: 0, crossarms: 0, insulator: 0 }
+      pole_data = {}
       next if json_file.to_s == '.'
       next if json_file.to_s == '..'
 
       json =  JSON.parse File.read "#{Rails.root}/lib/assets/" + json_file
       json['pole'].each do |coord|
-        pole_data[:xcoord] = coord[0]
-        pole_data[:ycoord] = coord[1]
+        pole_data[:xcoord] = coord[1][0]
+        pole_data[:ycoord] = coord[1][1]
       end
       json['esri_data']['assets'].each do |asset|
-        pole_data[:poles] = asset[0] unless asset[0].nil?
-        pole_data[:crossarms] = asset[1] unless asset[1].nil?
-        pole_data[:insulator] = asset[2] unless asset[2].nil?
+        pole_data[asset[0].to_sym] = asset[1]
       end
       poles.push(pole_data)
     end
